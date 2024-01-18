06fdfb563db49255439a204ec4db26a85cc8ae7b
Ajmal Hakim <samimajmal@yahoo.com>
2024-01-18 09:08:26 -0500

    add sol for linked list cyle ch

---
+++ src/main/java/linkedList/LinkedListCycle.java
@@ -0,0 +1,26 @@
+package linkedList;
+
+import java.util.List;
+
+public class LinkedListCycle {
+    class ListNode{
+        int value;
+        ListNode next;
+
+        public ListNode(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+    public boolean hasCycle(ListNode head){
+        ListNode fast = head;// moves by 2
+        ListNode slow = head;// moves by 1
+        while(fast!=null && fast.next !=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) return true;
+
+        }
+        return false;
+    }
+}
