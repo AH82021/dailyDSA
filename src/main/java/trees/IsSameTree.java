092a1223ccb9d5673e05dcb82ea9fe7b0312ab1b
Ajmal Hakim <samimajmal@yahoo.com>
2024-01-26 07:57:28 -0500

    add sol for isSameTree ch

---
+++ src/main/java/trees/IsSameTree.java
@@ -0,0 +1,34 @@
+package trees;
+
+public class IsSameTree {
+    class TreeNode {
+        int val;
+    TreeNode left;
+      TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left,TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    // TC O(p+q)
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+      return   dfs(p, q);
+
+    }
+
+    private boolean dfs(TreeNode p, TreeNode q) {
+        if(p == null & q == null) return true;
+
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        boolean left = dfs(p.left, q.left);
+        boolean right = dfs(p.right, q.right);
+
+        return left && right ;
+    }
+
+}
